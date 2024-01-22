1500112d13ef89be917e28d583e35cd0141b86af
AyushSingh4321 <ayushksb11@gmail.com>
2024-01-23 03:10:26 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
+import './categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        colorScheme: ThemeData().colorScheme.copyWith(secondary: Colors.amber),
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyLarge: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            bodyMedium: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            titleLarge: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold
+            )),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.pink,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: CategoriesScreen(),
+      routes : {
+        '/category-meals': (ctx) => CategoryMealsScreen()
+      },
+    );
+  }
+}
